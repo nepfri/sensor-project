af325c4ebf8618d83a1539cee8ef9155aea74179
Fritzner <nepfri@gmail.com>
2017-09-09 02:02:58 -0500

    Made some changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/temperature', function(req, res) {
-    res.send('24 C');
+    res.send('24 °C');
 });
 
 app.get('/humidity', function(req, res) {
